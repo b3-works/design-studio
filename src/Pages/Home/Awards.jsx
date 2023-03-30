6119b80bb23a21d4d1df0d3260d32c2063e99007
bonwooko <ko.bonwoo@gmail.com>
2023-03-30 17:35:46 +0900

    add json data files, awards section

--- src/Pages/Home/Awards.jsx
+++ src/Pages/Home/Awards.jsx
@@ -1,5 +1,40 @@
+import styled from 'styled-components';
+import Line from '../../components/Line';
+import awardsData from '../../data/awards.json';
+
+const Section = styled.section``;
+const Title = styled.h1``;
+const AwardsContainer = styled.div``;
+const Award = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+
+  div {
+    font-size: 1.5rem;
+  }
+`;
+
 const Awards = () => {
-  return <div>Awards</div>;
+  const data = awardsData.awards;
+
+  return (
+    <Section>
+      <Title>Awards</Title>
+      <AwardsContainer>
+        {data.map((award) => (
+          <>
+            <Award key={award.id}>
+              <div>{award.id}</div>
+              <div>{award.name}</div>
+              <div>{award.year}</div>
+            </Award>
+            <Line />
+          </>
+        ))}
+      </AwardsContainer>
+    </Section>
+  );
 };
 
 export default Awards;
